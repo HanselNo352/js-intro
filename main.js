5fc7bd384cc2d3667a088057d4f0cb844a413cde
HanselNo352 <HanselNo352@Northwested.org>
2026-03-10 12:59:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// Noah Hansel
+// 09 MAR 2026
+// Variable Declaration & Assignment
+
+//Declare variables 
+const firstName = "Noah";
+const lastName = "Hansel";
+const eyeColor = "Brown"
+const homeSchool = "Buckley High School";
+const careerTechProgram = "Web & App Development";
+const grade = "12";
+
+//Display the value stored in each variable
+
+console.log(firstName);
+console.log(lastName);
+console.log(eyeColor);
+console.log(homeSchool);
+console.log(careerTechProgram);
+console.log(grade);
